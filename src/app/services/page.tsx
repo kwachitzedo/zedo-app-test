1e488dd88f2b93aa165c71c01655582fd9bf868b
kwachitzedo <>
2025-04-02 02:28:30 +0530

    services section added

---
+++ src/app/services/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Image from "next/image";
+
+interface ImageData {
+	url: string;
+	name: string;
+}
+
+export default function Gallery() {
+	return (
+		<div className="flex  flex-col gap-4 mx-auto p-4 max-w-5xl">
+			<Image
+				width={1000}
+				height={800}
+				src={"/services.jpg"}
+				alt={"services"}
+				className={`object-contain w-auto max-h-[90vh] max-w-full `}
+			/>
+		</div>
+	);
+}
